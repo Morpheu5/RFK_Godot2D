21ba77cd05b1069beb697873423f442f96d84bb4
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-01-29 15:48:14 +0100

    Kind of OK

--- Actors/Robot.gd
+++ Actors/Robot.gd
@@ -9,9 +9,16 @@ var angle: = 0.0
 var angular_speed: = 1.5
 var active_actor: Actor = null
 
+func _ready():
+	$ActionButton.visible = false
+
 func _physics_process(delta: float) -> void:
 	angle += Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var direction: = get_direction().rotated(angle * delta * angular_speed)
+	if (direction.length() > 0):
+		get_node("RobotSprite").playing = true
+	else:
+		get_node("RobotSprite").playing = false
 	self.get_node("RobotSprite").set_rotation(angle * delta * angular_speed)
 	velocity = speed * delta * direction
 	velocity = move_and_slide(velocity)
@@ -29,12 +36,15 @@ func _process(delta):
 	if Input.is_action_just_released("ask_kitten") and active_actor != null:
 		print("<robot> Hey, ", active_actor, "... are you kitten?")
 		active_actor.is_kitten()
+		if $ActionButton.visible:
+			$ActionButton.visible = false
 
 func _on_BoxDetector_body_entered(body: Actor) -> void:
 	if body == self:
 		return
 	active_actor = body
 	emit_signal("see_box")
+	$ActionButton.visible = true
 	print("<robot> I see a mystery box...")
 
 func _on_BoxDetector_body_exited(body: Actor) -> void:
@@ -42,7 +52,8 @@ func _on_BoxDetector_body_exited(body: Actor) -> void:
 		return
 	active_actor = null
 	emit_signal("leave_box")
+	$ActionButton.visible = false
 	print("<robot> KHTXBAI")
 
 func _on_Robot_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	print(event.as_text())
+	print("------", event.as_text())
--- Stage.gd
+++ Stage.gd
@@ -67,8 +67,8 @@ func load_nkis():
 	nkis.shuffle()
 
 func _ready():
-	$HUD/MarginContainer/HBoxContainer/ButtonContainer/ActionButton.visible = false
-	$HUD/MarginContainer/HBoxContainer/LabelContainer/MarginContainer/Label.visible = false
+#	$HUD/MarginContainer/HBoxContainer/ButtonContainer/ActionButton.visible = false
+	$HUD/MarginContainer/Label.visible = false
 	randomize()
 	load_nkis()
 	load_symbols_textures()
@@ -91,15 +91,15 @@ func _ready():
 
 func _on_Robot_approach_box() -> void:
 	print("* Robot sees a box")
-	$HUD/MarginContainer/HBoxContainer/ButtonContainer/ActionButton.visible = true
+#	$HUD/MarginContainer/HBoxContainer/ButtonContainer/ActionButton.visible = true
 
 func _on_Robot_leave_box() -> void:
 	print("* Robot leaves a box")
-	$HUD/MarginContainer/HBoxContainer/ButtonContainer/ActionButton.visible = false
-	$HUD/MarginContainer/HBoxContainer/LabelContainer/MarginContainer/Label.text = ""
-	$HUD/MarginContainer/HBoxContainer/LabelContainer/MarginContainer/Label.visible = false
+#	$HUD/MarginContainer/HBoxContainer/ButtonContainer/ActionButton.visible = false
+	$HUD/MarginContainer/Label.text = ""
+	$HUD/MarginContainer/Label.visible = false
 
 func _on_Actor_is_kitten(is_kitten, what_is) -> void:
 	print("<box> ", what_is)
-	$HUD/MarginContainer/HBoxContainer/LabelContainer/MarginContainer/Label.text = what_is
-	$HUD/MarginContainer/HBoxContainer/LabelContainer/MarginContainer/Label.visible = true
+	$HUD/MarginContainer/Label.text = what_is
+	$HUD/MarginContainer/Label.visible = true
