3b0b7ddde142ba680099b3c4cf81bc9d54f0713e
Andrea Franceschini <andrea.franceschini@gmail.com>
2022-04-04 22:38:04 +0200

    Oops

--- Actors/MysteryBox.gd
+++ Actors/MysteryBox.gd
@@ -5,5 +5,5 @@ var what = ""
 
 func is_kitten():
 	# Uncomment this one if you want to mark boxes you've already opened
-	$BaseSprite.modulate = Color(1,1,1,1)
+	# $BaseSprite.modulate = Color(1,1,1,1)
 	emit_signal("is_kitten", false, what)
--- Makefile
+++ Makefile
@@ -10,4 +10,4 @@ html5: _md_html5
 	godot --no-window --path ./project.godot --export HTML5 build/html5/index.html \
 	&& cd build/html5 \
 	&& rm -f **/*.import \
-	&& 7za a -mx=9 html5.7z * 
+	&& 7zz a -mx=9 html5.7z * 
